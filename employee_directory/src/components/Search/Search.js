e3d639809ec181c489121fb9d527769ebe89fd5b
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2021-01-18 16:13:54 -0500

    added SearchResults component

--- employee_directory/src/components/Search/Search.js
+++ employee_directory/src/components/Search/Search.js
@@ -1,18 +1,21 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+
 
 function Search(props) {
     return (
         <form className="search">
             <div>
                 <input
-                value={props.searchEntry}
-                onChange={props.handleInputChange}
-                type="text"
-                className="form-control"
-                id="search"
-                placeholder="Type an employee's name to search"
+                    value={props.searchEntry}
+                    onChange={props.handleInputChange}
+                    type="text"
+                    className="form-control"
+                    id="search"
+                    placeholder="Type an employee's name to search"
                 />
-            </div>
+                <Button variant="dark" onClick={props.handleFomSubmit}>Submit</Button>
+                </div>
         </form>
     )
 };
---
+++ employee_directory/src/components/SearchResults/SearchResults.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Search from "../Search/Search";
+import Table from "react-bootstrap/Table";
+
+function SearchResults(props) {
+    return (
+        <div className="tableDiv">
+            {/* props.children */}
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>Photo</th>
+                        <th onClick={props.handleSortClick}>Name</th>
+                        <th onClick={props.handleSortClick}>Location</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.employeeList.map((employee) => (
+                        <tr>
+                            <td><img src={employee.picture.medium} /></td>
+                            <td>{employee.name.first} {employee.name.last}</td>
+                            <td>{employee.location.city} {employee.location.state}</td>
+                            <td>{employee.phone}</td>
+                            <td>{employee.email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </div>
+    )
+};
+
+export default SearchResults;
