99bfac84bcd99e8c9d64e367d74986009bf4b631
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2021-01-23 14:25:55 -0500

    updated employeeList prop

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,9 +1,9 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import api from "../utils/api";
-import Search from "../src/components/Search";
-import SearchResults from "../src/components/SearchResults";
-import Wrapper from "../src/components/Wrapper"
+import api from "./utils/api.js";
+import Search from "../src/components/Search/Search.js";
+import SearchResults from "../src/components/SearchResults/SearchResults.js";
+import Wrapper from "../src/components/Wrapper/Wrapper.js"
 
 class App extends React.Component {
 state = {
@@ -17,7 +17,7 @@ componentDidMount () {
     .then(res => {
       const employees = res.data.results;
       this.setState({
-        allUsers: employees,
+        allEmployees: employees,
         filteredEmployees: employees
       })
     })
@@ -33,12 +33,11 @@ render() {
   <div>
     <Wrapper>
       <Search searchEntry={this.state.searchEntry} handleInputChange={e => this.handleInputChange(e)}/>
-      <SearchResults />
+      <SearchResults employeeList={this.state.allEmployees}/>
     </Wrapper>
   </div>
   
   )}
 };
-
 
 export default App;
--- employee_directory/src/components/SearchResults/SearchResults.js
+++ employee_directory/src/components/SearchResults/SearchResults.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Search from "../Search/Search";
+// import Search from "../Search/Search.js";
 import Table from "react-bootstrap/Table";
 
 function SearchResults(props) {
@@ -19,7 +19,7 @@ function SearchResults(props) {
                 <tbody>
                     {props.employeeList.map((employee) => (
                         <tr>
-                            <td><img src={employee.picture.medium} /></td>
+                            <td><img src={employee.picture.medium} alt={employee.name.first}/></td>
                             <td>{employee.name.first} {employee.name.last}</td>
                             <td>{employee.location.city} {employee.location.state}</td>
                             <td>{employee.phone}</td>
